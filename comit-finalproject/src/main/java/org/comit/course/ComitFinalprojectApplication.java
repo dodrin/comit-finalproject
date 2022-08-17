2fbd69924df134fd9003f3e7a0ab0da048e84795
dodrin <indrn@icloud.com>
2022-08-16 21:23:45 -0700

    test commit

---
+++ comit-finalproject/src/main/java/org/comit/course/ComitFinalprojectApplication.java
@@ -0,0 +1,13 @@
+package org.comit.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComitFinalprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComitFinalprojectApplication.class, args);
+	}
+
+}
---
+++ comit-finalproject/src/test/java/org/comit/course/ComitFinalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.comit.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComitFinalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
